64631d167e7bc83b8ef4f3770d96f4d78752e714
Facundo Migliorini <facundo.migliorini@opskins.com>
2020-02-14 11:18:44 -0300

    Moving progress to git

---
+++ hola_mundo/handler.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hola ${event.pathParameters.name}`,
+        input: event.name,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.showUser = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Post data`,
+        input: event['body'],
+      },
+      null,
+      2
+    ),
+  };
+};
